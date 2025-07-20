bbd20e7f1f82ee940e7d0355d42e996717db7299
Poornima N <156445020+PoornimaNanjeGowda@users.noreply.github.com>
2025-07-20 11:22:15 +0530

    app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import numpy as np
 import joblib
 import plotly.express as px
 import io
+import os
+import gdown
 
-# Load model and metadata
-model = joblib.load("/content/best_model.pkl")
-trained_columns = joblib.load("/content/trained_columns.pkl")
-label_encoders = joblib.load("/content/label_encoders.pkl")
+# --- Load Model and Metadata from Google Drive ---
+MODEL_URL = "https://drive.google.com/file/d/1YcqF579snKy36ihiiWVsHJj4wkPponCF/view?usp=sharing"  # Replace with actual file ID
+MODEL_PATH = "/content/best_model.pkl"
+TRAINED_COLUMNS_PATH = "/content/trained_columns.pkl"
+LABEL_ENCODERS_PATH = "/content/label_encoders.pkl"
+
 
 # USD to INR conversion
 USD_TO_INR = 83.5
