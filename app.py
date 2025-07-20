a048602a2c080402b5a4d5291829075d64f776ed
Poornima N <156445020+PoornimaNanjeGowda@users.noreply.github.com>
2025-07-20 12:00:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+
 import plotly.express as px
 import io
 import os
@@ -10,8 +10,6 @@ import gdown
 # --- Load Model and Metadata from Google Drive ---
 MODEL_URL = "https://drive.google.com/file/d/1YcqF579snKy36ihiiWVsHJj4wkPponCF/view?usp=sharing"  # Replace with actual file ID
 MODEL_PATH = "/content/best_model.pkl"
-
-model = joblib.load('/content/best_model.pkl')
 TRAINED_COLUMNS_PATH = "/content/trained_columns.pkl"
 LABEL_ENCODERS_PATH = "/content/label_encoders.pkl"
 
